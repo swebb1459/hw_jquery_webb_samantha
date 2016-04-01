d54f49f2ca1e66042e6801b5d65eb2f8b4b56cdb
Samantha Webb <samanthawebb@outlook.com>
2016-04-01 19:00:26 -0400

    add css styling to paragraphs with jQuery

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 
     var paragraphs = $( 'p' );
 
+    $(paragraphs).css({
+        'color': 'blue',
+        'font-style': 'italic',
+        'line-height': '200%'
+    });
+
 });
